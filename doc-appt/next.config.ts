bfeace81df5eeb7b01d07bbf910830e3e6864377
Srishti <srishti_lulla@brown.edu>
2025-03-04 20:18:39 -0500

    deploy

--- doc-appt/next.config.ts
+++ doc-appt/next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // Outputs a Single-Page Application (SPA)
   distDir: "build", // Changes the build output directory to `build`
-  
+  reactStrictMode: true,
+  basePath: "/doctor-appointments"
 };
 
 export default nextConfig;
